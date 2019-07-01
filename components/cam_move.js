668c25332e4f075000b0aa41178c0d65782069e2
gegelascience <remi.verchere2@gmail.com>
2019-07-01 14:43:42 +0200

    add params and cam animation slower

--- components/cam_move.js
+++ components/cam_move.js
@@ -1,51 +1,54 @@
 AFRAME.registerComponent("cam_move", {
   schema: {
-    evol: { type: "number", default: 0 }
+    evol: { type: "number", default: 0 },
+    x_animation_time: { type: "number", default: 12000 },
+    z_animation_time: { type: "number", default: 25000 }
   },
   init: function () {
-    document.addEventListener("blur", event => {
-      this.data.evol = 0;
-      var pos = {
-        x: 0,
-        y: 1.5,
-        z: 138
-      };
-      this.el.setAttribute("position", pos);
-    });
+    this.lapTime = 2 * this.data.x_animation_time + 2 * this.data.z_animation_time;
+    this.p1 = this.data.x_animation_time / 2;
+    this.p2 = this.p1 + this.data.z_animation_time;
+    this.p3 = this.p2 + this.data.x_animation_time;
+    this.p4 = this.p3 + this.data.z_animation_time;
   },
 
   tick: function (time, timeDelta) {
     this.data.evol += timeDelta;
-    var state = this.data.evol % 62000;
+    var state = this.data.evol % this.lapTime;
     var pos = this.el.getAttribute("position");
-    if (state <= 5250) {
-      pos.x += (20 * timeDelta) / 5250;
+    if (state <= this.p1) {
+      pos.x += (20 * timeDelta) / (this.data.x_animation_time / 2);
       if (pos.x > 20) {
         pos.x = 20;
+        this.data.evol = this.p1 + 1;
       }
       this.el.setAttribute("position", pos);
-    } else if (state <= 25750) {
-      pos.z -= (128 * timeDelta) / 20500;
+    } else if (state <= this.p2) {
+      pos.z -= (128 * timeDelta) / this.data.z_animation_time;
       if (pos.z < 10) {
         pos.z = 10;
+        this.data.evol = this.p2 + 1;
       }
       this.el.setAttribute("position", pos);
-    } else if (state <= 36250) {
-      pos.x -= (40 * timeDelta) / 10500;
+    } else if (state <= this.p3) {
+      pos.x -= (40 * timeDelta) / this.data.x_animation_time;
       if (pos.x < -20) {
         pos.x = -20;
+        this.data.evol = this.p3 + 1;
       }
       this.el.setAttribute("position", pos);
-    } else if (state <= 56750) {
-      pos.z += (128 * timeDelta) / 20500;
+    } else if (state <= this.p4) {
+      pos.z += (128 * timeDelta) / this.data.z_animation_time;
       if (pos.z > 138) {
         pos.z = 138;
+        this.data.evol = this.p4 + 1;
       }
       this.el.setAttribute("position", pos);
     } else {
-      pos.x += (20 * timeDelta) / 5250;
+      pos.x += (20 * timeDelta) / (this.data.x_animation_time / 2);
       if (pos.x > 0) {
         pos.x = 0;
+        this.data.evol = 0;
       }
       this.el.setAttribute("position", pos);
     }
